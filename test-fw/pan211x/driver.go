512a6eb44985d35effb07209eebaa924d1f9b7a0
Pavel Burgr <pb@drake.cz>
2026-04-28 21:28:38 +0200

    refactor: enhance DumpState function for improved debugging output

--- test-fw/pan211x/driver.go
+++ test-fw/pan211x/driver.go
@@ -479,24 +479,210 @@ func (d *Driver) Receive(buf []byte) (n int, ok bool) {
 	return int(length), true
 }
 
-// DumpState prints key register values over RTT for debugging.
+// DumpState prints key register values with decoded field meanings for debugging.
 func (d *Driver) DumpState() {
-	dump := func(name string, reg uint8) {
-		v, err := d.registers.Read(reg)
+	r := d.registers
+	println("--- PAN211x ---")
+
+	rd := func(reg uint8) (uint8, bool) {
+		v, err := r.Read(reg)
 		if err != nil {
-			println(name, "ERR")
-		} else {
-			println(name, v)
+			return 0, false
+		}
+		return v, true
+	}
+
+	if v, ok := rd(STATE_CFG); ok {
+		s := "unknown"
+		switch v {
+		case STATE_STB3:
+			s = "STB3"
+		case STATE_TX:
+			s = "TX"
+		case STATE_RX:
+			s = "RX"
+		case STATE_SLEEP:
+			s = "SLEEP"
+		case STATE_STB3_INIT:
+			s = "STB3_INIT"
+		}
+		println("STATE     :", s)
+	}
+
+	if v, ok := rd(SPI_CFG); ok {
+		en := "0"
+		if v&0x80 != 0 {
+			en = "1"
+		}
+		println("SPI_CFG   : 3wire_ren=" + en)
+	}
+
+	if v, ok := rd(WMODE_CFG0); ok {
+		crc := "OFF"
+		switch v & 0xC0 {
+		case CRC_1B:
+			crc = "1B"
+		case CRC_2B:
+			crc = "2B"
+		case CRC_3B:
+			crc = "3B"
+		}
+		mode := "XN297L"
+		switch v & 0x30 {
+		case WORK_MODE_FS01:
+			mode = "FS01"
+		case WORK_MODE_FS32:
+			mode = "FS32/BLE"
+		}
+		whiten, endian := "0", "LE"
+		if v&WHITEN_EN_BIT != 0 {
+			whiten = "1"
+		}
+		if v&ENDIAN_BIG != 0 {
+			endian = "BE"
+		}
+		println("WMODE_CFG0: crc=" + crc + " mode=" + mode + " whiten=" + whiten + " endian=" + endian)
+	}
+
+	if v, ok := rd(WMODE_CFG1); ok {
+		rxgoon, fifo, dpy, enh := "0", "64B", "0", "0"
+		if v&RX_GOON_BIT != 0 {
+			rxgoon = "1"
+		}
+		if v&FIFO_128_BIT != 0 {
+			fifo = "128B"
+		}
+		if v&DPY_EN_BIT != 0 {
+			dpy = "1"
+		}
+		if v&ENHANCE_BIT != 0 {
+			enh = "1"
+		}
+		addr := "?"
+		switch v & 0x03 {
+		case ADDR_2B:
+			addr = "2B"
+		case ADDR_3B:
+			addr = "3B"
+		case ADDR_4B:
+			addr = "4B"
+		case ADDR_5B:
+			addr = "5B"
+		}
+		println("WMODE_CFG1: rx_goon=" + rxgoon + " fifo=" + fifo + " dpy=" + dpy + " enh=" + enh + " addr=" + addr)
+	}
+
+	if v, ok := rd(RXPIPE_CFG); ok {
+		pipes := ""
+		for i := uint8(0); i < 6; i++ {
+			if v>>i&1 != 0 {
+				pipes += string([]byte{'0' + i})
+			}
+		}
+		if pipes == "" {
+			pipes = "(none)"
+		}
+		println("RXPIPE_CFG: enabled=" + pipes)
+	}
+
+	if v, ok := rd(RF_CHANNEL_CFG); ok {
+		println("CHANNEL   : ch=", v, "/ freq=", 2400+int(v), "MHz")
+	}
+
+	{
+		rx, ok1 := rd(RXPLLEN_CFG)
+		tx, ok2 := rd(TXPLLEN_CFG)
+		if ok1 && ok2 {
+			println("PAYLOAD   : rx=", rx, "tx=", tx, "bytes")
 		}
 	}
-	dump("STATE_CFG ", STATE_CFG)
-	dump("WMODE_CFG0", WMODE_CFG0)
-	dump("WMODE_CFG1", WMODE_CFG1)
-	dump("RF_CHANNEL", RF_CHANNEL_CFG)
-	dump("RFIRQFLG  ", RFIRQFLG)
-	dump("RFIRQ_CFG ", RFIRQ_CFG)
-	dump("STATUS0   ", STATUS0)
-	dump("STATUS3   ", STATUS3)
-	dump("RXPIPE_CFG", RXPIPE_CFG)
-	dump("DATARATE  ", RF_DATARATE_CFG)
+
+	if v, ok := rd(RF_DATARATE_CFG); ok {
+		rate := "1Mbps"
+		switch v & DATARATE_BW_MASK {
+		case DATARATE_BW_2MBPS:
+			rate = "2Mbps"
+		case DATARATE_BW_250KBPS:
+			rate = "250kbps"
+		}
+		println("DATARATE  :", rate)
+	}
+
+	if v, ok := rd(TRXMODE_CFG); ok {
+		tx := "SINGLE"
+		if v&TX_CONTINUOUS_BIT != 0 {
+			tx = "CW"
+		}
+		rx := "SINGLE"
+		switch v & 0x60 {
+		case RX_TIMEOUT_BIT:
+			rx = "TIMEOUT"
+		case RX_CONTINUOUS_BIT:
+			rx = "CONT"
+		}
+		presync := "0"
+		if v&PRE_SYNC_EN_BIT != 0 {
+			presync = "1"
+		}
+		println("TRXMODE   : tx=" + tx + " rx=" + rx + " presync=" + presync)
+	}
+
+	if v, ok := rd(RFIRQFLG); ok {
+		flags := irqNames(v)
+		println("RFIRQFLG  : set=" + flags)
+	}
+
+	if v, ok := rd(RFIRQ_CFG); ok {
+		masked := irqNames(v)
+		println("RFIRQ_CFG : masked=" + masked)
+	}
+
+	if v, ok := rd(STATUS0); ok {
+		pipe := (v & STATUS0_PIPE_MASK) >> STATUS0_PIPE_SHIFT
+		cierr := "0"
+		if v&STATUS0_CI_ERR_BIT != 0 {
+			cierr = "1"
+		}
+		extra := ""
+		if pipe == STATUS0_PIPE_EMPTY>>STATUS0_PIPE_SHIFT {
+			extra = " (FIFO empty)"
+		}
+		println("STATUS0   : pipe=" + string([]byte{'0' + pipe}) + " ci_err=" + cierr + extra)
+	}
+
+	if v, ok := rd(STATUS3); ok {
+		println("STATUS3   : rxlen=", v)
+	}
+}
+
+func irqNames(v uint8) string {
+	s := ""
+	if v&IRQ_TX != 0 {
+		s += " TX"
+	}
+	if v&IRQ_MAX_RT != 0 {
+		s += " MAX_RT"
+	}
+	if v&IRQ_ADDR_ERR != 0 {
+		s += " ADDR_ERR"
+	}
+	if v&IRQ_CRC_ERR != 0 {
+		s += " CRC_ERR"
+	}
+	if v&IRQ_LEN_ERR != 0 {
+		s += " LEN_ERR"
+	}
+	if v&IRQ_PID_ERR != 0 {
+		s += " PID_ERR"
+	}
+	if v&IRQ_RX_TIMEOUT != 0 {
+		s += " RX_TO"
+	}
+	if v&IRQ_RX != 0 {
+		s += " RX"
+	}
+	if s == "" {
+		return "(none)"
+	}
+	return s[1:] // trim leading space
 }
