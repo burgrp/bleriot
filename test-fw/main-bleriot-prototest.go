a7eb6cb8176d2e359013c5a1ac7f577864adf7fe
Pavel Burgr <pb@drake.cz>
2026-05-03 22:53:49 +0200

    refactor Device structure to remove Role type and enhance initialization methods

--- test-fw/main-bleriot-prototest.go
+++ test-fw/main-bleriot-prototest.go
@@ -19,30 +19,24 @@ const (
 	// pinSpiCsn  = machine.PA10 // CSN  → PAN211x pin 1, active-low
 )
 
-type Role bool
-
-const (
-	RoleHub  Role = false
-	RoleNode Role = true
-)
-
-var roleNames = map[Role]string{
-	RoleHub:  "Hub",
-	RoleNode: "Node",
+type Radio interface {
+	SetChannel(channel uint8) error
+	EnableRxAddress(index uint8, addr [4]byte) error
+	Receive(buf []byte) (int, bool)
+	Send(addr [4]byte, data []byte) error
 }
 
 type Device struct {
-	role       Role
 	myAddr     [4]byte
 	peerAddr   [4]byte
 	pinSpiSck  machine.Pin
 	pinSpiData machine.Pin
 	pinSpiCsn  machine.Pin
 	led        machine.Pin
+	radio      Radio
 }
 
-func (d *Device) start() {
-	println("Initializing", roleNames[d.role])
+func (d *Device) initialize() {
 
 	d.led.High()
 	time.Sleep(500 * time.Millisecond)
@@ -50,46 +44,115 @@ func (d *Device) start() {
 
 	radio := pan211x.NewDriverBLELongRange(pan211x.NewRegistersSPI(spi.NewMaster(d.pinSpiSck, d.pinSpiData), d.pinSpiCsn))
 
-	d.must(radio.Init(pan211x.ConfigBLELongRange{
+	must(radio.Init(pan211x.ConfigBLELongRange{
 		PayloadLen:      bleriot.PacketLen,
 		SerialInterface: pan211x.SerialInterfaceSPI3W,
 		SpreadFactor:    pan211x.SpreadFactorS8,
 	}))
 
-	d.must(radio.SetChannel(11))
-	d.must(radio.EnableRxAddress(0, d.myAddr))
+	must(radio.SetChannel(11))
+	must(radio.EnableRxAddress(0, d.myAddr))
 
-	println(roleNames[d.role], "initialized")
-
-	nextPacket := time.Now()
-
-	var buf [bleriot.PacketLen]byte
-	for {
-		n, ok := radio.Receive(buf[:])
-		if ok {
-			println(roleNames[d.role], "received", n, "bytes:", buf[:n])
-			d.led.Set(!d.led.Get())
-		}
-
-		if time.Now().After(nextPacket) {
-			err := radio.Send(d.peerAddr, []byte{0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF})
-			if err != nil {
-				println(roleNames[d.role], "transmit error:", err.Error())
-			} else {
-				println(roleNames[d.role], "transmitted packet")
-			}
-			nextPacket = time.Now().Add(500 * time.Millisecond)
-		}
-
-		runtime.Gosched()
-	}
+	d.radio = radio
 
 }
 
-func (d *Device) must(err error) {
-	if err != nil {
-		panic(roleNames[d.role] + ": " + err.Error())
+func (d *Device) startHub() {
+	d.initialize()
+}
+
+type TestRegisters struct {
+	registers map[RegisterId]Register
+}
+
+func (r *TestRegisters) GetRegisterById(id RegisterId) (Register, bool) {
+	reg, ok := r.registers[id]
+	return reg, ok
+}
+
+func (r *TestRegisters) PrintDescriptor(indent int) {
+	for id, reg := range r.registers {
+		println("Register ID:", id)
+		reg.PrintDescriptor(indent + 2)
 	}
+}
+
+type IntRegister struct {
+	value int32
+}
+
+func (r *IntRegister) GetRawValue() int32 {
+	return r.value
+}
+
+func (r *IntRegister) PrintDescriptor(indent int) {
+	println("IntRegister with value", r.value)
+}
+
+func (d *Device) startNode() {
+	d.initialize()
+
+	registers := &TestRegisters{
+		registers: map[RegisterId]Register{},
+	}
+	registers.registers[5] = &IntRegister{value: 42}
+
+	node := NewNode(d.radio, registers, d.myAddr, [16]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10})
+
+	must(node.run())
+}
+
+type Node struct {
+	radio     Radio
+	registers Registers
+	address   [4]byte
+	key       [4]uint32
+}
+
+type Register interface {
+	GetRawValue() int32
+	PrintDescriptor(indent int)
+}
+
+type RegisterId uint16
+
+type Registers interface {
+	GetRegisterById(id RegisterId) (Register, bool)
+	PrintDescriptor(indent int)
+}
+
+func NewNode(radio Radio, registers Registers, address [4]byte, key [16]byte) *Node {
+
+	n := &Node{
+		radio:     radio,
+		registers: registers,
+		address:   address,
+	}
+
+	for i := range n.key {
+		n.key[i] = uint32(key[i*4]) | uint32(key[i*4+1])<<8 |
+			uint32(key[i*4+2])<<16 | uint32(key[i*4+3])<<24
+	}
+
+	return n
+}
+
+func (n *Node) run() error {
+
+	println("--- bleriot-node")
+	print("address: 0x")
+	printAddress(n.address)
+	println()
+
+	print("key: \"")
+	printKey(n.key)
+	println("\"")
+
+	n.registers.PrintDescriptor(0)
+
+	println("...")
+	return nil
+
 }
 
 func main() {
@@ -98,28 +161,26 @@ func main() {
 	pinLedGreen.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	pinLedRed.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	hubAddr := [4]byte{0x00, 0x00, 0x00, 0x00}
-	nodeAddr := [4]byte{0x00, 0x00, 0x00, 0x01}
+	hubAddr := [4]byte{0xCC, 0xA0, 0x00, 0x01}
+	nodeAddr := [4]byte{0xCC, 0xA0, 0x00, 0x02}
 
 	go (&Device{
-		role:       RoleHub,
 		myAddr:     hubAddr,
 		peerAddr:   nodeAddr,
 		pinSpiSck:  machine.PA9,
 		pinSpiData: machine.PA7,
 		pinSpiCsn:  machine.PA10,
 		led:        pinLedRed,
-	}).start()
+	}).startHub()
 
 	go (&Device{
-		role:       RoleNode,
 		myAddr:     nodeAddr,
 		peerAddr:   hubAddr,
 		pinSpiSck:  machine.PB4,
 		pinSpiData: machine.PB3,
 		pinSpiCsn:  machine.PB5,
 		led:        pinLedGreen,
-	}).start()
+	}).startNode()
 
 	for {
 		printMemoryStats()
@@ -137,5 +198,34 @@ func printMemoryStats() {
 func must(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func printSpace(len int) {
+	for i := 0; i < len; i++ {
+		print(" ")
+	}
+}
+
+var hexDigits = [16]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
+
+func printAddress(addr [4]byte) {
+	for _, b := range addr {
+		h := b >> 4
+		l := b & 0xF
+		print(hexDigits[h])
+		print(hexDigits[l])
+	}
+}
+
+func printKey(key [4]uint32) {
+	for _, k := range key {
+		for i := 0; i < 4; i++ {
+			b := byte(k >> (8 * uint(i)))
+			h := b >> 4
+			l := b & 0xF
+			print(hexDigits[h])
+			print(hexDigits[l])
+		}
 	}
 }
